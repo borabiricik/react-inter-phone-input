fb6605708fa87e5f5b38b356009d80fa3c486004
borabiricik <bora.biricik>
2022-11-05 15:35:46 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./app-style.css";
+import { Hello } from "react-inter-phone-input";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Hello />
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
