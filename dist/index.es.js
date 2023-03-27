16864c86a607b58a63040666acbfc6771d3bc56c
borabiricik <bora.biricik>
2023-03-27 12:01:51 +0300

    feat: example project & storybook implementation

---
+++ dist/index.es.js
@@ -0,0 +1,1 @@
+import{jsx as t}from"react/jsx-runtime";const e=e=>t("div",Object.assign({className:"bg-red-400"},{children:"Button2"}));export{e as Button};
---
+++ dist/index.js
@@ -0,0 +1,1 @@
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0});var e=require("react/jsx-runtime");exports.Button=t=>e.jsx("div",Object.assign({className:"bg-red-400"},{children:"Button2"}));
