79f9aa7fae2836f6b33e758ef817e196e7a57bb4
borabiricik <bora.biricik>
2023-03-27 17:49:00 +0300

    Add test case for MobileNumber component in MobileNumber.test.tsx

--- src/components/MobileNumber/MobileNumber.test.tsx
+++ src/components/MobileNumber/MobileNumber.test.tsx
@@ -1,6 +1,12 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+
+import { MobileNumber } from './MobileNumber';
+
+//TODO: Remove jest & add cypress
+
 describe('Button', () => {
-  // it('should render button with text', () => {
-  //   const { getByText } = render(<Button text="test" />);
-  //   expect(getByText('test')).toBeTruthy();
-  // });
+  it('should render button with text', () => {
+    render(<MobileNumber />);
+  });
 });
