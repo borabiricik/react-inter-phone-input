3fe8dc5bca42b6e9feee8eaf3c26f9e0baf296b8
borabiricik <bora.biricik>
2023-03-27 17:36:35 +0300

    Refactor MobileNumber component to replace button with div

--- src/components/MobileNumber/MobileNumber.tsx
+++ src/components/MobileNumber/MobileNumber.tsx
@@ -3,5 +3,5 @@ import React from 'react';
 export interface MobileNumberProps {}
 
 export const MobileNumber: React.FC<MobileNumberProps> = () => {
-  return <button className="bg-red-200">asdasd</button>;
+  return <div>asd</div>;
 };
