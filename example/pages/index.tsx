e858c0c68f022ee5e8b98572bfde0d40d0c3bea5
borabiricik <bora.biricik>
2023-03-29 16:24:27 +0300

    feat: example code update

--- example/pages/index.tsx
+++ example/pages/index.tsx
@@ -1,8 +1,22 @@
 import type { NextPage } from 'next';
+import { useState } from 'react';
 import { MobileNumber } from 'react-inter-phone-input';
 
 const Home: NextPage = () => {
-  return <div>{/* <MobileNumber  /> */}</div>;
+  const [phoneNumber, setphoneNumber] = useState('');
+  const [selectedCountry, setselectedCountry] = useState('+90');
+  return (
+    <div>
+      <MobileNumber
+        value={{
+          dialCode: selectedCountry,
+          phoneNumber,
+        }}
+        onPhoneNumberChange={(number) => setphoneNumber(number)}
+        onCountryChange={(dialCode) => setselectedCountry(dialCode)}
+      />
+    </div>
+  );
 };
 
 export default Home;
